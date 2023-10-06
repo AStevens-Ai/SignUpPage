b832c7c842d9389c5030e9ddf04602c1ee0cfab6
Alexander Stevens <murakumo@Alexanders-MacBook-Pro.local>
2023-10-06 16:06:13 -0400

    added console.log and spaced out fields

--- src/SignUpForm.jsx
+++ src/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function SignUpForm() {
     const [name, setName] = useState("");
@@ -27,10 +27,12 @@ export default function SignUpForm() {
         if (name === "" || email === "" || password === "") {
             setError(true);
             window.alert("Please Enter Details in all fields");
+            console.log('invalid')
         } else {
             setSubmitted(true);
             setError(false);
             window.alert(`Congratulations, ${name} you have been successfully registered!!`);
+            console.log([name, email, password])
         }
     };
 
@@ -42,14 +44,20 @@ export default function SignUpForm() {
 
             <form>
                 <label className="label">Name</label>
+                <br />
                 <input onChange={handleName} className="input" value={name} type="text" />
-
+                <br />
+                <br />
                 <label className="label">Email</label>
+                <br />
                 <input onChange={handleEmail} className="input" value={email} type="email" />
-
+                <br />
+                <br />
                 <label className="label">Password</label>
+                <br />
                 <input onChange={handlePassword} className="input" value={password} type="password" />
-
+                <br />
+                <br />
                 <button onClick={handleSubmit} className="btn" type="submit">
                     Submit
                 </button>
